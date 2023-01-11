0b01af57c30667b050edf288b29db8a937a796ff
Illia_Polishchuk <illoy168@gmail.com>
2023-01-11 20:09:49 +0200

    Initial commit

---
+++ MyTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
